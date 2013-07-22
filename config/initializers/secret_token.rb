d2e46d0f8adcc3b0afa7a390ae7c51272c30cfb8
Jurgens Smit <contact@jurgenssmit.com>
2013-07-22 14:54:10 +1000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinkedinAuth::Application.config.secret_token = '19157cd88a443a69ab4160128af5e633f832adcb089977e28d158864ecd2749b96ec7f7aa044be8079e8f00200c350793054347e745256db53028c5e9100f814'
